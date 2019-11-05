3c096e717094a7dbfe8e6e5c03963def63b882a1
shumaikunkun <s1711520@gmail.com>
2019-11-06 02:04:48 +0900

    PHPでGitHubのAPIが呼べるようになった

--- oss.php
+++ oss.php
@@ -17,12 +17,47 @@
     <?php  ?>
 
     <?php
-    $url="https://api.github.com/users/shumaikunkun/repos?per_page=100&page=1";
-    $json = file_get_contents($url);
-    $arr = json_decode($json);
 
-    echo $json;
-    ?>
+// $url = "https://api.github.com/orgs/docker";
+// // cURLセッションを初期化
+// $ch = curl_init();
+// // オプションを設定
+// curl_setopt($ch, CURLOPT_URL, $url); // 取得するURLを指定
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // 実行結果を文字列で返す
+// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // サーバー証明書の検証を行わない
+// // URLの情報を取得
+// $response =  curl_exec($ch);
+// // 取得結果を表示
+// echo $response;
+// $result = json_decode($response, true);
+// // セッションを終了
+// curl_close($conn);
+
+
+// $token = '';
+// $query = '';
+// $options = [
+//     'http' => [
+//         'method' => 'POST',
+//         'header' => [
+//             'User-Agent: My User Agent',
+//             'Authorization: bearer '.$token,
+//             'Content-type: application/json; charset=UTF-8',
+//         ],
+//         'content' => json_encode(['query' => $query]),
+//     ],
+// ];
+// $context = stream_context_create($options);
+// $contents = file_get_contents('https://api.github.com/graphql', false, $context);
+// var_dump(json_decode($contents));
+
+
+ini_set('user_agent','UserAgent');
+$json = file_get_contents('https://api.github.com/orgs/docker');
+$arr = json_decode($json,true);
+echo $arr["name"]
+
+?>
 
     </p>
 
