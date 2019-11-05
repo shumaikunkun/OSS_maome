c262d33a3037276ee5c2998bceab55902bb7b1bd
shumaikunkun <s1711520@gmail.com>
2019-11-06 04:18:55 +0900

    各組の羅列可能に

--- oss.php
+++ oss.php
@@ -16,23 +16,25 @@
     <?php  ?>
 
 <?php
+$orgs=["google","aws","apple","facebook","microsoft"];
+foreach($orgs as $org){
 ini_set('user_agent','UserAgent');
-$json = file_get_contents('https://api.github.com/orgs/github');
+$json = file_get_contents('https://api.github.com/orgs/'.$org);
 $arr = json_decode($json,true);
 $member_json = file_get_contents(str_replace("{/member}","",$arr["public_members_url"]));
 $member_arr = json_decode($member_json,true);
 ?>
 
-<img src=<?php echo $arr["avatar_url"]."&s=100" ?> />
+<img src=<?php echo $arr["avatar_url"]."&s=100" ?> /><br>
 <p><?php echo $arr["name"] ?></p>
 <p><?php echo $arr["location"] ?></p>
-<p><?php echo $arr["html_url"] ?></p>
-<p><?php echo $arr["blog"] ?></p>
+<a href=<?php echo $arr["html_url"] ?> > <?php echo $arr["html_url"] ?> </a>
+<a href=<?php echo $arr["blog"] ?> > <?php echo $arr["blog"] ?> </a>
 <p><?php echo count($member_arr) ?></p>
 <p><?php echo $arr["public_repos"] ?></p>
 <p><?php echo explode("-",$arr["created_at"])[0] ?>年</p>
 
-
+<?php } ?>
 
   </body>
 </html>
